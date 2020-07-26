7952afe88215d66333e9fd2ad033e4ed127770c0
Sebastian Hernandez <sebhernandezram@gmail.com>
2020-07-26 12:19:17 -0500

    add tailwind config files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 const App = () => {
-  return <div>MI CAFÉ VERDE</div>;
+  return (
+    <div>
+      <h1 className="bg-red-500">MI CAFÉ VERDE</h1>
+    </div>
+  );
 };
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React, { StrictMode } from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+import "assets/main.css";
+import App from "App";
 
 ReactDOM.render(
   <StrictMode>
